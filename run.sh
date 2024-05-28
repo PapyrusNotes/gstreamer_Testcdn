290b92c4c80487c5e31148d92e1ed267331f533f
PapyrusNotes <chris170841@gmail.com>
2024-05-28 18:28:16 +0900

    <build> fix build object

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 docker run -it -d -p 5000:5000 \
-  -v $(pwd):/home/infer1 \
+  -v ./testcdn:/home/infer1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=unix$DISPLAY \
   --name testcdn \
   testcdn
-
-docker exec -it testcdn bash
