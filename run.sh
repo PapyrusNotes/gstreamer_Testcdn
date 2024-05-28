5c39a7f21ae99875afce40755f3fd73304aac26d
PapyrusNotes <chris170841@gmail.com>
2024-05-28 18:23:09 +0900

    <build>: add build elements

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-docker run -d -p 5000:5000
+docker run -it -d -p 5000:5000 \
   -v $(pwd):/home/infer1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=unix$DISPLAY \
-  --name testcdn
+  --name testcdn \
+  testcdn
 
 docker exec -it testcdn bash
