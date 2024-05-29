73e516200e143bacd2fc9ab19e9fc5131f775714
PapyrusNotes <chris170841@gmail.com>
2024-05-29 18:34:44 +0900

    <>

--- main.py
+++ main.py
@@ -52,9 +52,8 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
 
 
 os.chdir(hls_dir)
-#server_address = ('', 5001)
-#httpd = HTTPServer(server_address, CORSRequestHandler)
-print("Serving HLS on port 5001...")
+server_address = ('', 6003)
+httpd = HTTPServer(server_address, CORSRequestHandler)
 
 
 Gst.init(sys.argv)
@@ -68,5 +67,5 @@ gpipeline.start(main_loop)
 pipeline = gpipeline.pipeline
 pipeline.set_state(Gst.State.PLAYING)
 
-# httpd.serve_forever()
+httpd.serve_forever()
 
