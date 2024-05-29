d0e3dc8c884495c0bec8549c26303f60584fd169
PapyrusNotes <chris170841@gmail.com>
2024-05-29 13:37:09 +0900

    <>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker run -it -d -p 5001:5001 \
   -v ./:/home/infer1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /hls:hls\
+  -v /hls:/home/infer1/hls\
   -e DISPLAY=unix$DISPLAY \
   --name testcdn \
   testcdn
