53c045652e2199b84dcc5e39669d630d4ff67ea2
PapyrusNotes <chris170841@gmail.com>
2024-05-29 14:51:45 +0900

    <test>: testing original indexhtml

--- main.py
+++ main.py
@@ -2,38 +2,6 @@ import os
 import sys
 import subprocess
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from flask import Flask, send_from_directory
-
-app = Flask(__name__)
-
-UPLOAD_FOLDER_0 = '/hls/0/'
-UPLOAD_FOLDER_1 = '/hls/1/'
-UPLOAD_FOLDER_2 = '/hls/2/'
-UPLOAD_FOLDER_3 = '/hls/3/'
-app.config['UPLOAD_FOLDER_0'] = UPLOAD_FOLDER_0
-app.config['UPLOAD_FOLDER_1'] = UPLOAD_FOLDER_1
-app.config['UPLOAD_FOLDER_2'] = UPLOAD_FOLDER_2
-app.config['UPLOAD_FOLDER_3'] = UPLOAD_FOLDER_3
-
-
-@app.route('/stream/0/<filename>')
-def serve_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER_0'], filename)
-
-
-@app.route('/stream/1/<filename>')
-def serve_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER_1'], filename)
-
-
-@app.route('/stream/2/<filename>')
-def serve_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER_2'], filename)
-
-
-@app.route('/stream/3/<filename>')
-def serve_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER_3'], filename)
 
 
 import gi
@@ -46,14 +14,14 @@ from pipeline.gresource.gpipeline import GPipeline
 # Paths
 # input_video = 'input.mp4'
 hls_dir = 'hls'
-output_playlist = os.path.join('hls/list.m3u8')
+output_playlist = os.path.join('hls/0/output.m3u8')
 print(output_playlist)
 
 # Create HLS directory if it doesn't exist
 os.makedirs(hls_dir, exist_ok=True)
 
 # Create index.html
-index_html_content = '''<!DOCTYPE html>
+index_html_content ='''<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -63,59 +31,15 @@ index_html_content = '''<!DOCTYPE html>
 </head>
 <body>
     <video-js id="my_video" class="vjs-default-skin" controls preload="auto" width="640" height="360"></video-js>
-    <script src="https://cdn.jsdelivr.net/npm/hls.js@1"></script>
-<video id="video"></video>
-<script>
-  var video = document.getElementById('video');
-  var videoSrc = 'http://210.206.5.147:5001/hls/0/output.m3u8';
-  if (Hls.isSupported()) {
-    var hls = new Hls();
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-  }
-  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoSrc;
-  }
-</script>
-<video id="video"></video>
-<script>
-  var video = document.getElementById('video');
-  var videoSrc = 'http://210.206.5.147:5001/hls/1/output.m3u8';
-  if (Hls.isSupported()) {
-    var hls = new Hls();
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-  }
-  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoSrc;
-  }
-</script>
-<video id="video"></video>
-<script>
-  var video = document.getElementById('video');
-  var videoSrc = 'http://210.206.5.147:5001/hls/2/output.m3u8';
-  if (Hls.isSupported()) {
-    var hls = new Hls();
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-  }
-  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoSrc;
-  }
-</script>
-<video id="video"></video>
-<script>
-  var video = document.getElementById('video');
-  var videoSrc = 'http://210.206.5.147:5001/hls/3/output.m3u8';
-  if (Hls.isSupported()) {
-    var hls = new Hls();
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-  }
-  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoSrc;
-  }
-</script>
+    <script src="https://vjs.zencdn.net/7.10.2/video.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.14.1/videojs-contrib-hls.js"></script>
+    <script>
+        var player = videojs('my_video');
+        player.src({
+            src: 'list.m3u8',
+            type: 'application/x-mpegURL'
+        });
+    </script>
 </body>
 </html>'''
 
@@ -123,12 +47,12 @@ with open(os.path.join(hls_dir, 'index.html'), 'w') as f:
     f.write(index_html_content)
 
 # Serve the HLS content
-'''
+
 os.chdir(hls_dir)
 server_address = ('', 5001)
 httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
 print("Serving HLS on port 5001...")
-'''
+
 
 Gst.init(sys.argv)
 Gst.debug_set_active(True)
@@ -141,7 +65,4 @@ gpipeline.start(main_loop)
 pipeline = gpipeline.pipeline
 pipeline.set_state(Gst.State.PLAYING)
 
-# httpd.serve_forever()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+httpd.serve_forever()
