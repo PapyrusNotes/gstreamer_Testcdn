cb0cd57c0587043c203576702fc349706e974bb2
PapyrusNotes <chris170841@gmail.com>
2024-06-11 16:30:11 +0900

    <>

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
 
     Gst.init(sys.argv)
     Gst.debug_set_active(True)
-    Gst.debug_set_default_threshold(3)
+    Gst.debug_set_default_threshold(4)
     main_loop = GLib.MainLoop()
 
     # Gstreamer Main Loop Task를 Python Thread에 할당
