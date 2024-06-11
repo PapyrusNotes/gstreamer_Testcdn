cc22ef21ef15d877ed70b68896d8867e544b34ca
PapyrusNotes <chris170841@gmail.com>
2024-06-11 18:20:46 +0900

    <test>: Testing One camera stream

--- pipeline/cfg/cfgs.py
+++ pipeline/cfg/cfgs.py
@@ -1,13 +1,14 @@
 RTSP_SRC = ["rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/00/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/01/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/02/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/06/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/04/media.smp",
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/01/media.smp",
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/02/media.smp",
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/06/media.smp",
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/04/media.smp",
             # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/07/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/08/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/09/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/10/media.smp",
-            "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/12/media.smp",]
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/08/media.smp",
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/09/media.smp",
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/10/media.smp",
+            # "rtsp://admin:daolcnc3470@211.225.156.126:558/LiveChannel/12/media.smp",
+            ]
 
 # SRC_CFG = {'framerate': '30/1', 'max-rate': 5}
 
