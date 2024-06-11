2748cce2b5a7e310df1943042f2b9b78e63ec818
PapyrusNotes <chris170841@gmail.com>
2024-06-11 18:08:45 +0900

    <>

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-docker run -it -d -p 6003:6003 \
+docker run --gpus all -it -d -p 6003:6003 \
   -v ./:/home/infer1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /hls:/home/infer1/hls\
   -e DISPLAY=unix$DISPLAY \
-  -- gpus all
   --name testcdn \
   testcdn
