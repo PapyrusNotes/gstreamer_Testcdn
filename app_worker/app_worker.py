441d24e36314fbca8f7ada3031c7aeda699263b1
PapyrusNotes <chris170841@gmail.com>
2024-06-12 14:51:05 +0900

    <test>: testing mlmodel output

--- app_worker/app_worker.py
+++ app_worker/app_worker.py
@@ -30,6 +30,7 @@ class AppWorker:
             img_array = get_ndarray(sample)
 
             self.mlmodel.predict(img_array=img_array, input_shape=(960, 960))
+            print(self.mlmodel.predict_result)
 
             obj_tensor_result = self.mlmodel.extract_tensor()
             frame.set_obj_result(obj_tensor_result)
