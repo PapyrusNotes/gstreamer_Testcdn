065e1508cf91701073e071a24ba156d96e422aa0
PapyrusNotes <chris170841@gmail.com>
2024-06-13 20:01:29 +0900

    <test>: adjust infer_queue Appworker

--- app_worker/app_worker.py
+++ app_worker/app_worker.py
@@ -21,6 +21,8 @@ class AppWorker:
 
     def process_imaging(self):
         try:
+            if infer_queue.qsize() < 1:
+                time.sleep(0.5)
             print("Main App : infer_queue size : ", infer_queue.qsize())
             frame = infer_queue.get(timeout=0)
             sample = frame.get_sample()
