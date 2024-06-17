f59e7d3e577d95195fb5d87a1df505f92decca73
PapyrusNotes <chris170841@gmail.com>
2024-06-17 11:07:24 +0900

    <test>: Downgrading Resolution size FULLHD to HD

--- pipeline/gresource/constructor.py
+++ pipeline/gresource/constructor.py
@@ -88,8 +88,13 @@ class AppSinkConstructor:
         parse = Gst.ElementFactory.make("h264parse", "parse2")
         avdec = Gst.ElementFactory.make("avdec_h264", "decode")  # x-264 to x-raw
         convert = Gst.ElementFactory.make("videoconvert", "convert")
+        videoscale = Gst.ElementFactory.make("videoscale", "videoscale")
+        videorate = Gst.ElementFactory.make("videorate", "videorate")
+        videorate.set_property("drop-only", True)
+        videorate.set_property("max-rate", 20)
+        videorate.set_property("silent", True)
 
-        caps = Gst.Caps.from_string(f"video/x-raw, format=(string)RGB, width=(int)1920, height=(int)1080")
+        caps = Gst.Caps.from_string(f"video/x-raw, format=(string)RGB, width=(int)1280, height=(int)720")
 
         appsink = Gst.ElementFactory.make("appsink", f"appsink-{self.index}")
         appsink.set_property("emit-signals", True)
@@ -103,6 +108,8 @@ class AppSinkConstructor:
         Gst.Bin.add(new_bin, parse)
         Gst.Bin.add(new_bin, avdec)
         Gst.Bin.add(new_bin, convert)
+        Gst.Bin.add(new_bin, videoscale)
+        Gst.Bin.add(new_bin, videorate)
         Gst.Bin.add(new_bin, appsink)
 
         def on_pad_added(element1, pad, element2):
@@ -125,10 +132,23 @@ class AppSinkConstructor:
         if ret:
             print("AppSink Bin : avdec - convert connected")
 
+        ret = ret and convert.link(videoscale)
+        if ret:
+            print("AppSink Bin : convert - videoscale connected")
+
+        ret = ret and videoscale.link(videorate)
+        if ret:
+            print("AppSink Bin : videoscale - videorate connected")
+
+        ret = ret and videorate.link_filtered(appsink, caps)
+        if ret:
+            print("AppSink Bin : convert - videoscale connected")
+
+        '''
         ret = ret and convert.link_filtered(appsink, caps)
         if ret:
             print("AppSink Bin : convert - caps - appsink connected")
-
+        '''
         if not ret:
             print("AppSink Bin ERROR: Elements could not be linked")
             sys.exit(1)
@@ -155,16 +175,18 @@ class InferHLSConstructor:
         appsrc.set_property("emit-signals", True)
         appsrc.connect("need-data", on_start_feed, self.index)
         appsrc.connect("enough-data", on_halt_feed, self.index)
-        caps = Gst.Caps.from_string(f"video/x-raw, format=RGB, width=1920, height=1080")
+        caps = Gst.Caps.from_string(f"video/x-raw, format=RGB, width=1280, height=720")
         appsrc.set_property("caps", caps)
 
         convert = Gst.ElementFactory.make("videoconvert", "convert")
         overlay = Gst.ElementFactory.make("cairooverlay", "overlay")
-        overlay.connect('draw', on_draw, self.index)
+        overlay.connect('draw', on_draw, self.index)  # video/x-raw
 
         convert2 = Gst.ElementFactory.make("videoconvert", "convert2")
+        videoscale = Gst.ElementFactory.make("videoscale")
+        videorate = Gst.ElementFactory.make("videorate")
 
-        x264enc = Gst.ElementFactory.make("x264enc", "x264enc")
+        x264enc = Gst.ElementFactory.make("x264enc", "x264enc")  # video/x-264
         x264enc.set_property("tune", "zerolatency")
         x264enc.set_property("bitrate", 1200)
         x264enc.set_property("speed-preset", 4)
@@ -183,6 +205,8 @@ class InferHLSConstructor:
         Gst.Bin.add(new_bin, overlay)
         Gst.Bin.add(new_bin, convert2)
         Gst.Bin.add(new_bin, x264enc)
+        Gst.Bin.add(new_bin, videoscale)
+        Gst.Bin.add(new_bin, videorate)
         Gst.Bin.add(new_bin, mpegtsmux)
         Gst.Bin.add(new_bin, hlssink)
 
@@ -197,12 +221,25 @@ class InferHLSConstructor:
         ret = overlay.link(convert2)
         if ret:
             print("InferHLS Bin : overlay - convert2 connected")
-
+        '''
         ret = convert2.link(x264enc)
         if ret:
             print("InferHLS Bin : convert2 - x264enc connected")
+        '''
 
-        ret = ret and x264enc.link(mpegtsmux)
+        ret = convert2.link(videoscale)
+        if ret:
+            print("InferHLS Bin : convert2 - videoscale connected")
+
+        ret = videoscale.link(videorate)
+        if ret:
+            print("InferHLS Bin : videoscale - videorate connected")
+
+        ret = ret and videorate.link_filtered(caps, x264enc)
+        if ret:
+            print("InferHLS Bin : videorate - x264enc connected")
+
+        ret = x264enc.link(mpegtsmux)
         if ret:
             print("InferHLS Bin : x264enc - mpegtsmux connected")
 
